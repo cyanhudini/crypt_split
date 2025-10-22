2c6046544b78f304280a04d31563cf47bc24b682
debian <n.rudolphi11@gmail.com>
2025-10-22 16:07:19 +0200

    create skeleton for chunking

--- src/split.rs
+++ src/split.rs
@@ -1,5 +1,6 @@
-use std::path::PathBuf;
-
+use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
+use std::fs::File;
 
 pub struct FileChunk {
     pub index: usize,
@@ -7,9 +8,32 @@ pub struct FileChunk {
     pub size: usize,
 }
 
-fn split_file(){}
+const CHUNK_SIZE: usize = 4096;
 
-fn hash_chunk(){}
+fn split_file<P: AsRef<PathBuf>>(file_path: P,output_path: P) -> io::Result<Vec<FileChunk>> {
+
+    /*
+    die gechnkten dateien sollen in einen ordner gespeichert werden, der name des ordners ist eine uuid
+    der user sollte den pfad angeben wo der ordner erstellt werden soll
+
+
+    */
+    let mut file = File::open(file_path.as_ref())?;
+    let mut chunks: Vec<FileChunk> = Vec::new();
+    let mut buffer: Vec<u8> = Vec::with_capacity(CHUNK_SIZE);
+
+    let mut index = 0;
+    loop {
+        
+    }
+    Ok(chunks)
+
+}
+
+fn hash_chunk(){
+
+
+}
 
 #[cfg(test)]
 mod tests {}
