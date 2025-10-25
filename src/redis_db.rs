7a9d7818b821839fa578b93b1adc9fff101fe24f
debian <n.rudolphi11@gmail.com>
2025-10-25 21:03:07 +0200

    adding  a bare minimum redis client

--- src/redis_db.rs
+++ src/redis_db.rs
@@ -1,5 +1,22 @@
-use redis;
 use dotenv::dotenv;
+use redis::{self, Commands};
+use std::env;
 
+pub struct RedisClient {
+    client: redis::Client,
+}
 
+impl RedisClient {
+   
+    pub fn create_from_env() -> redis::RedisResult<Self> {
+        dotenv().ok();
+        let url = env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1/".to_string());
+        let client = redis::Client::open(url.as_str())?;
+
+        let mut con = client.get_connection()?;
+
+        Ok(RedisClient { client })
+    }
+
+}
 
