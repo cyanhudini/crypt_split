f4fdf23c825bad2045a006ffc8d49bc66b054786
debian <n.rudolphi11@gmail.com>
2025-10-26 15:57:29 +0100

    add delete_key function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod redis_db;
 mod key_management;
 mod cloud;
 
+fn split_file_store_metadata(){}
+
 fn main() {
     println!("Hello, world!");
 }
--- src/redis_db.rs
+++ src/redis_db.rs
@@ -1,6 +1,6 @@
 use crate::split::FileChunk;
 use dotenv::dotenv;
-use redis::{self, Client, TypedCommands};
+use redis::{self, Client, TypedCommands, RedisResult};
 use serde_json;
 use std::env;
 
@@ -11,14 +11,14 @@ pub struct RedisClient {
 }
 // TODO: implementiere SET, GET, DELETE und Basic Auth, Connection sollte langlebig sein
 impl RedisClient {
-    pub fn create_from_env() -> redis::RedisResult<Self> {
+    pub fn create_from_env() -> RedisResult<Self> {
         dotenv().ok();
         let url = env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1/".to_string());
         let client = Client::open(url)?;
         let conn = client.get_connection()?;
         Ok(Self { connection: conn })
     }
-
+    //TODO: file_hash sollte später der datei name sein, return type eventuell zu () bzw. kännte es zum logging benutzt werden
     pub fn set_hvalue(
         &mut self,
         file_hash: &str,
@@ -30,6 +30,11 @@ impl RedisClient {
         return res
         
     }
+
+    pub fn delete_hkey(&mut self, file_hash: &str) -> RedisResult<usize> {
+        let res = self.connection.del(file_hash)?;
+        Ok(res)
+    }
 }
 
 #[cfg(test)]
@@ -37,6 +42,9 @@ mod tests {
     use super::*;
     use std::env;
     use std::path::PathBuf;
+
+
+    //TODO: füge Test hinzu um hset mit einer echten Test Datei zu überprüfen
 
     #[test]
     fn test_set_key_value() {
